790f6382ee116657da8f9e37bc57cb9240bcba89
Ashar Farooq <ashar744@gmail.com>
2021-08-06 05:57:01 -0400

    creating a new window

--- snake_game.py
+++ snake_game.py
@@ -3,3 +3,15 @@ import curses
 
 
 stdscr = curses.initscr()
+curses.curs_set(2)
+screen_dimensions = stdscr.getmaxyx()
+screen_width = screen_dimensions[0]
+screen_height = screen_dimensions[1]
+
+new_game_window = curses.newwin(screen_height,screen_width,0,0)
+
+new_game_window.keypad(1)
+new_game_window.timeout(110)
+
+snake_x = screen_width/8
+snake_y = screen_height/4
