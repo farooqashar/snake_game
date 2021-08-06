d9a2bf60568ea8fc87365d637be6fa9aa409534c
Ashar Farooq <ashar744@gmail.com>
2021-08-06 06:42:49 -0400

    added diamonds

--- snake_game.py
+++ snake_game.py
@@ -1,35 +1,66 @@
-import random 
+import random
 import curses
 
-
 stdscr = curses.initscr()
-curses.curs_set(2)
-screen_dimensions = stdscr.getmaxyx()
-screen_width = screen_dimensions[0]
-screen_height = screen_dimensions[1]
+curses.curs_set(0)
+height, width = stdscr.getmaxyx()
 
-new_game_window = curses.newwin(screen_height,screen_width,0,0)
-
+new_game_window = curses.newwin(height, width, 0, 0)
 new_game_window.keypad(1)
-new_game_window.timeout(110)
+new_game_window.timeout(125)
 
-snake_x = screen_width/8
-snake_y = screen_height/4
-
+snake_x = width/2
+snake_y = height/8
 snake = [
-    [snake_y,snake_x],
-    [snake_y-1,snake_x],
-    [snake_y-2,snake_x],
-    [snake_y-3,snake_x]
+    [snake_y, snake_x],
+    [snake_y, snake_x-1],
+    [snake_y, snake_x-2],
+    [snake_y, snake_x-3],
+    [snake_y, snake_x-4]
 ]
 
-target = (screen_width/4, screen_height/4)
-target_x = target[0]
-target_y = target[1]
+target = [height/2, width/2]
+new_game_window.addch(int(target[0]), int(target[1]), curses.ACS_DIAMOND)
 
-new_game_window.addch(target_y,target_x, curses.ACS_DIAMOND)
-
-key = curses.KEY_LEFT
+key = curses.KEY_DOWN
 
 while True:
+    next_key = new_game_window.getch()
+    key = key if next_key == -1 else next_key
+
+    if snake[0][0] in [0, height] or snake[0][1]  in [0, width] or snake[0] in snake[1:]:
+        curses.endwin()
+        print("You lost :(")
+        quit()
+
+    new_head = [snake[0][0], snake[0][1]]
+
+    if key == curses.KEY_DOWN:
+        new_head[0] += 1
+    if key == curses.KEY_UP:
+        new_head[0] -= 1
+    if key == curses.KEY_LEFT:
+        new_head[1] -= 1
+    if key == curses.KEY_RIGHT:
+        new_head[1] += 1
+
+    snake.insert(0, new_head)
+
+    if snake[0] == target:
+        target = None
+        while target is None:
+            new_target = [
+                random.randint(1, height-1),
+                random.randint(1, width-1)
+            ]
+            target = new_target if new_target not in snake else None
+        new_game_window.addch(target[0], target[1], curses.ACS_DIAMOND)
+    else:
+        snake_end = snake.pop()
+        new_game_window.addch(int(snake_end[0]), int(snake_end[1]), ' ')
+
+    new_game_window.addch(int(snake[0][0]), int(snake[0][1]), curses.ACS_CKBOARD)
+
+
+if __name__ == "__main__":
     pass
